76de370a441d1918f85ea59526f9691e2b280c2e
shahar <shahar@budgetao.com>
2017-06-25 17:06:47 +0300

    slack integration

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-const base64 = require('base-64');
 const Datastore = require('@google-cloud/datastore');
-
-const ds = Datastore();
+const Slack = require('slack-node');
+const base64 = require('base-64');
 
 exports.pubsubLogSink = function (event, callback) {
     console.log(`Function input: ${JSON.stringify(event)}`);
     let data = JSON.parse(base64.decode(event.data.data));
     console.log(`Data: ${JSON.stringify(data)}`);
 
-    getConfig().then(config => {
-        console.log(`Config: ${JSON.stringify(config)}`);
-        callback();
-    });
+    getConfig()
+        .then(config => {
+            console.log(`Config: ${JSON.stringify(config)}`);
+            return sendSlack(config.slackChannel, 'Test Message', config.slackAPIToken);
+        })
+        .then(() => {
+            callback();
+        });
 };
 
 function getConfig() {
+    const ds = Datastore();
     const query = ds.createQuery(['Config']);
-    return runDSQuery(query).then(configsArray => {
+    return runDSQuery(ds,query).then(configsArray => {
         return configsArray.reduce((configs, config) => {
             configs[config.name] = config.value;
             return configs;
@@ -24,7 +28,7 @@ function getConfig() {
     });
 }
 
-function runDSQuery(query) {
+function runDSQuery(ds,query) {
     return new Promise((resolve, reject) => {
         ds.runQuery(query, (err, entities, nextQuery) => {
             if (err) {
@@ -32,12 +36,29 @@ function runDSQuery(query) {
             }
             const hasMore = nextQuery.moreResults !== Datastore.NO_MORE_RESULTS ? nextQuery.endCursor : false;
             if (hasMore) {
-                runDSQuery(nextQuery).then(moreEntities => {
+                runDSQuery(ds,nextQuery).then(moreEntities => {
                     resolve(entities.concat(moreEntities));
                 });
             }
             else {
                 resolve(entities);
+            }
+        });
+    });
+}
+
+function sendSlack(channel, message, apiToken) {
+    return new Promise((resolve, reject) => {
+        const slack = new Slack(apiToken);
+        slack.api('chat.postMessage', {
+            text: message,
+            channel: channel
+        }, function (err, response) {
+            if (!!err) {
+                reject(err);
+            }
+            else {
+                resolve(response);
             }
         });
     });
